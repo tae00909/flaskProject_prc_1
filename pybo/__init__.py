9da9ae9f916616dee80a6220de78810316c75666
rlaxogud0909 <rlaxogud0909@likelion.org>
2021-11-02 13:16:17 +0900

    플라스크 라이트 함수

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -9,11 +9,15 @@ def create_app():
     # __name__이라는 변수에는 모듈명이 담긴다.
     # 즉, 이 파일이 실행되면 app.py라는 모듈이 실행되는 것이므로 __name__ 변수에는 "app" 라는 문자열이 담긴다
     app = Flask(__name__)
-    # print(__name__) -> pybo 출력
-    # @app.route는 특정 URL에 접속하면 바로 다음 줄에 있는 함수를 호출하는 플라스크의 데코레이터
-    @app.route('/')
-    def hello_pybo():
-        return 'Hello, pybo!'
+    # # print(__name__) -> pybo 출력
+    # # @app.route는 특정 URL에 접속하면 바로 다음 줄에 있는 함수를 호출하는 플라스크의 데코레이터
+    # @app.route('/')
+    # def hello_pybo():
+    #     return 'Hello, pybo!'
+
+    from .views import main_views
+    # 블루 프린트를 사용하도록 변경
+    app.register_blueprint(main_views.bp)
 
     return app
 
---
+++ pybo/views/main_views.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+# 블루 프린트
+# 'main'(이름), __name__(모듈명), url_prefix='/'(URL 프리픽스)
+# URL 프리픽스는 특정 파일(main_views.py)에 있는 함수의 애너테이션 URL 앞에 기본으로 붙일 접두어 URL을 의미한다.
+# 예를 들어 main_views.py 파일의 URL 프리픽스에 url_prefix='/' 대신 url_prefix='/main'이라고 입력했다면
+# hello_pybo 함수를 호출하는 URL은 localhost:5000/이 아니라 localhost:5000/main/이 된다
+bp = Blueprint('main', __name__, url_prefix='/')
+
+# app.route -> bp.route
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return 'Pybo, index'
